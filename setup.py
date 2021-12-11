73543554ba57ad0d744a111f6798c063b2cfc3f7
fulder <misad90@gmail.com>
2021-12-11 18:01:40 +0100

    Look for packages in root package dir

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    package_dir={"": "src"},
-    packages=setuptools.find_packages(where="src"),
+    package_dir={"": "."},
+    packages=setuptools.find_packages(),
     python_requires=">=3.9",
 )
