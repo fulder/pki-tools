bee8005a889adb29a6feb433521c11537051b542
fulder <misad90@gmail.com>
2021-12-11 18:04:25 +0100

    Test bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="crl-checker",
-    version="0.0.1",
+    version="0.0.2",
     author="Michal Sadowski",
     author_email="misad90@gmail.com",
     description="Check if certificate is revoked using the x509 CRL extension",
