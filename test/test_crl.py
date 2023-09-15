005385a5b45107efdb2cb615e9fea88ff834bc3b
fulder <misad90@gmail.com>
2023-09-15 11:00:04 +0200

    Merge tests

--- test/test_crl.py
+++ test/test_crl.py
@@ -1,56 +1,11 @@
 import pytest
-from cryptography.hazmat.primitives import serialization
 
 
 from pki_tools.exceptions import (
-    ExtensionMissing,
     CrlFetchFailure,
     CrlLoadError,
 )
 from pki_tools.crl import is_revoked_pem
-from conftest import _create_cert, _create_crl
-
-
-def test_not_revoked_cert(key_pair, mocked_requests_get, cert_pem_string):
-    crl = _create_crl(key_pair, [])
-    crl_der = crl.public_bytes(serialization.Encoding.DER)
-
-    mocked_requests_get.return_value.status_code = 200
-    mocked_requests_get.return_value.content = crl_der
-
-    is_revoked_pem(cert_pem_string)
-
-
-def test_not_revoked_cert_pem_crl(
-    key_pair, mocked_requests_get, cert_pem_string
-):
-    crl = _create_crl(key_pair, [])
-    crl_pem = crl.public_bytes(serialization.Encoding.PEM)
-
-    mocked_requests_get.return_value.status_code = 200
-    mocked_requests_get.return_value.content = crl_pem
-
-    is_revoked_pem(cert_pem_string)
-
-
-def test_is_revoked_revoked_cert(
-    key_pair, mocked_requests_get, cert, cert_pem_string
-):
-    crl = _create_crl(key_pair, [cert.serial_number])
-    crl_der = crl.public_bytes(serialization.Encoding.DER)
-
-    mocked_requests_get.return_value.status_code = 200
-    mocked_requests_get.return_value.content = crl_der
-
-    assert is_revoked_pem(cert_pem_string)
-
-
-def test_cert_missing_crl_extension(key_pair):
-    cert = _create_cert(key_pair, add_crl_extension=False)
-    cert_pem = cert.public_bytes(serialization.Encoding.PEM).decode()
-
-    with pytest.raises(ExtensionMissing):
-        is_revoked_pem(cert_pem)
 
 
 def test_crl_fetch_error(mocked_requests_get, cert_pem_string):
--- test/test_ocsp.py
+++ test/test_ocsp.py
@@ -1,48 +1,9 @@
-import datetime
-
 import pytest
 from cryptography.hazmat.primitives import serialization
-from cryptography.x509 import ocsp
 
 from pki_tools.exceptions import ExtensionMissing, OcspFetchFailure
 from pki_tools.ocsp import is_revoked_pem
-from conftest import _create_cert, _create_mocked_ocsp_response
-
-
-def test_not_revoked_cert(
-    mocked_requests_get, cert, key_pair, cert_pem_string
-):
-    mocked_requests_get.return_value.status_code = 200
-    mocked_requests_get.return_value.content = _create_mocked_ocsp_response(
-        cert, key_pair
-    )
-
-    is_revoked_pem(cert_pem_string, cert_pem_string)
-
-
-def test_not_revoked_cert_pem(
-    mocked_requests_get, cert_pem_string, cert, key_pair
-):
-    mocked_requests_get.return_value.status_code = 200
-    mocked_requests_get.return_value.content = _create_mocked_ocsp_response(
-        cert, key_pair
-    )
-
-    is_revoked_pem(cert_pem_string, cert_pem_string)
-
-
-def test_is_revoked_revoked_cert(
-    key_pair, mocked_requests_get, cert, cert_pem_string
-):
-    mocked_requests_get.return_value.status_code = 200
-    mocked_requests_get.return_value.content = _create_mocked_ocsp_response(
-        cert,
-        key_pair,
-        status=ocsp.OCSPCertStatus.REVOKED,
-        revocation_time=datetime.datetime.now(),
-    )
-
-    assert is_revoked_pem(cert_pem_string, cert_pem_string)
+from conftest import _create_cert
 
 
 def test_cert_missing_extension(key_pair):
