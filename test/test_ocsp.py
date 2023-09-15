c7b697ed29c736adab0efc38973142dcb44b58b1
fulder <misad90@gmail.com>
2023-09-15 14:16:59 +0200

    Remove duplicate test

--- test/test_ocsp.py
+++ test/test_ocsp.py
@@ -1,15 +1,7 @@
 import pytest
 
-from pki_tools.exceptions import ExtensionMissing, OcspFetchFailure
+from pki_tools.exceptions import OcspFetchFailure
 from pki_tools.ocsp import is_revoked
-from conftest import _create_cert
-
-
-def test_cert_missing_extension(key_pair):
-    cert = _create_cert(key_pair, add_aia_extension=False)
-
-    with pytest.raises(ExtensionMissing):
-        is_revoked(cert, cert)
 
 
 def test_ocsp_fetch_error(mocked_requests_get, cert):
