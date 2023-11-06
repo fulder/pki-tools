de3a5f4169543672a0cb6b775f619ecdd8c20070
fulder <misad90@gmail.com>
2023-11-06 18:50:48 +0100

    Add apitests

---
+++ apitest/test_is_revoked.py
@@ -0,0 +1,16 @@
+import pki_tools
+from pki_tools import Certificate, Chain
+
+
+def test_revoked(revoked_cert, chain):
+    assert pki_tools.is_revoked(revoked_cert, chain)
+
+
+def test_not_revoked(cert, chain):
+    assert not pki_tools.is_revoked(cert, chain)
+
+
+def test_crl(intermediate_ca_pem, root_ca_pem):
+    cert = Certificate.from_pem_string(intermediate_ca_pem)
+    chain = Chain.from_pem_string(root_ca_pem)
+    assert not pki_tools.is_revoked(cert, chain)
