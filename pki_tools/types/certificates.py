98d86a900f9984180718b74671bb7b6967933dbf
Michał <misad90@gmail.com>
2023-11-06 15:39:40 +0100

    Fix from_uri certificates bug

--- pki_tools/types/certificates.py
+++ pki_tools/types/certificates.py
@@ -110,7 +110,7 @@ class Certificates(CryptoParser):
                 f"Issuer URI fetch failed. Status: {ret.status_code}"
             )
 
-        return cls(certificates=x509.load_pem_x509_certificate(ret.content))
+        return cls(certificates=[x509.load_pem_x509_certificate(ret.content)])
 
     @property
     def pem_string(self):
