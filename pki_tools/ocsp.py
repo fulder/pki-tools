578ae46263492a63340315bf6e8f6bc893cc1af2
Michał <misad90@gmail.com>
2023-11-08 10:00:12 +0100

    Format ocsp.py

--- pki_tools/ocsp.py
+++ pki_tools/ocsp.py
@@ -112,7 +112,7 @@ def _check_ocsp_status(
             "UniformResourceIdentifier: "
         )[1]
 
-        cache_ttl = round(time.time() / ocsp_res_cache_seconds )
+        cache_ttl = round(time.time() / ocsp_res_cache_seconds)
         ocsp_res = _get_ocsp_status(f"{server}/{req_path}", cache_ttl=cache_ttl)
 
         _verify_ocsp_signature(ocsp_res, issuer_chain)
