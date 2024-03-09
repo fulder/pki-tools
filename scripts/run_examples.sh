9b97704a5e42e70b98692ad026903ab41edc0b4b
fulder <misad90@gmail.com>
2024-03-09 13:46:42 +0100

    Add bash scripts for running examples

---
+++ scripts/run_examples.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
+cd ./docs/examples/src
+
+error_flag=0
+for file in *.py; do
+  echo "Running $file..."
+  output=$(python "$file" 2>&1)
+
+  # Check the exit code of the Python command
+  if [ $? -eq 0 ]; then
+    echo "$file executed successfully"
+  else
+    printf "${RED}Error executing $file${NC}\n"
+  error_flag=1
+  fi
+done
+
+exit $error_flag
