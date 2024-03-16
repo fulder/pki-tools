0b26021147dadf2364a490489731eea16c48015b
fulder <misad90@gmail.com>
2024-03-16 15:19:26 +0100

    Exclude recreating some out files

--- scripts/run_examples.sh
+++ scripts/run_examples.sh
@@ -5,12 +5,19 @@ NC='\033[0m' # No Color
 
 cd ./docs/examples/src
 
+exclude_outputs=("create_cert.py" "create_csr.py")
+
 error_flag=0
 for file in *.py; do
   echo "Running $file..."
 
   out_file="${file%.*}.out"
-  LOGURU_LEVEL=INFO poetry run python3 "$file" > ${out_file} 2>&1
+
+  if [[ "${exclude_outputs[@]}" =~ "${file}" ]]; then
+    LOGURU_LEVEL=INFO poetry run python3 "$file" 2>&1
+  else
+    LOGURU_LEVEL=INFO poetry run python3 "$file" > ${out_file} 2>&1
+  fi
 
   # Check the exit code of the Python command
   if [ $? -eq 0 ]; then
