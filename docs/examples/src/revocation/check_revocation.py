732848499486fb5b0c110e25cdc21ad7d79baf4d
Michal Sadowski <misad90@gmail.com>
2026-02-12 10:33:33 +0100

    Change lets encrypt CA to R13

--- docs/examples/src/revocation/check_revocation.py
+++ docs/examples/src/revocation/check_revocation.py
@@ -3,8 +3,7 @@ from pki_tools import Certificate, Chain, is_revoked
 chain = Chain.from_uri(
     [
         "https://letsencrypt.org/certs/isrgrootx1.pem",
-        "https://letsencrypt.org/certs/2024/r11.pem",
-        "https://letsencrypt.org/certs/2024/r10.pem",
+        "https://letsencrypt.org/certs/2024/r13.pem",
     ]
 )
 
