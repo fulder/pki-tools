2437eb06e599e5bdec160d92ead0490e58aca304
fulder <misad90@gmail.com>
2026-06-24 00:14:23 +0200

    Update example chains

--- docs/examples/src/chain/load_chain_uri.py
+++ docs/examples/src/chain/load_chain_uri.py
@@ -2,8 +2,9 @@ from pki_tools import Chain
 
 chain = Chain.from_uri(
     [
+        "https://letsencrypt.org/certs/gen-y/int-yr1.pem",
+        "https://letsencrypt.org/certs/gen-y/root-yr-by-x1.pem",
         "https://letsencrypt.org/certs/isrgrootx1.pem",
-        "https://letsencrypt.org/certs/lets-encrypt-r3.pem",
     ]
 )
 
